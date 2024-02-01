287a0eff06a2b8f76206f18ae4391cc003f27a3b
Ole Magnus Fon Johnsen <me@omfj.no>
2024-02-01 10:38:32 +0100

    Init

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{tsx,mdx}", "./theme.config.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
